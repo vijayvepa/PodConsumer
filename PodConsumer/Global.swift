e981eae4a51124c18fe858ebd0fddc783090e585
Vijay Vepakomma <vijay.vepakomma@philips.com>
2018-04-08 10:57:44 -0400

    Added consumption of pod code

---
+++ PodConsumer/Global.swift
@@ -0,0 +1,14 @@
+//
+//  Global.swift
+//  PodConsumer
+//
+//  Created by VIJAY VEPAKOMMA on 4/8/18.
+//  Copyright © 2018 VIJAY VEPAKOMMA. All rights reserved.
+//
+
+import Foundation
+import YLogging
+
+let logger = YLog()
+
+
